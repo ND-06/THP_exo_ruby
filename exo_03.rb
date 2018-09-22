3bec382d2c3a0893d8bd0a1053372328b2af5715
Natdenice <Clad1987@hotmail.com>
2018-09-22 17:00:32 +0200

    Ajout de l'exo_03.rb et du readme_exo_03.txt

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts("Bonjour, monde!")
+#Et avec une voix sexy, ca donne : Bonjour, monde!")
+
+# Ici, le symbole # permet d'inscrire un commentaire de manière à expliquer le code, et du coup, 
+# cela masque forcément la deuxieme ligne!
---
+++ readme_exo_03.txt
@@ -0,0 +1,2 @@
+# Ici, le symbole # permet d'inscrire un commentaire de manière à expliquer le code, et du coup, 
+# cela masque forcément la deuxieme ligne!
